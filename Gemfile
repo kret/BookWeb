477bafc4f97e9f6233f9479614e511f93f297e1e
kret <andreskee@gmail.com>
2011-04-09 13:16:07 +0200

    Clean Rails application skeleton for BookWeb

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3'
+gem 'rmagick'
+gem 'carrierwave'
+
+group :development do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'autotest'
+  gem 'autotest-rails'
+  gem 'ffaker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookWeb::Application.config.secret_token = '9e49beaeb2475e8fb8b5281c3b20013af4c06c24ba7a9244c04a4bd9ea2fecc99d534cd6aa4fd31e9063ec73a8cea454511f77e3d9617242f0b97c88f908a87b'

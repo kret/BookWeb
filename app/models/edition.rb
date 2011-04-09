89bf4dfa276198580bba7d02278759c95c280740
kret <andreskee@gmail.com>
2011-04-09 13:49:35 +0200

    Fixed Edition model typo

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -1,10 +1,10 @@
 class Edition < ActiveRecord::Base
   attr_accessible :title, :isbn, :edition_year, :issue_number, :language, :number_of_tomes
 
-  validates :title,       :presence => true
-  validates :isbn,        :presence => true
-  validates :editon_year, :presence => true
-  validates :language,    :presence => true
+  validates :title,         :presence => true
+  validates :isbn,          :presence => true
+  validates :edition_year,  :presence => true
+  validates :language,      :presence => true
 
   belongs_to :publication
 end

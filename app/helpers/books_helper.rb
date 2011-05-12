39c80f9b0c4f8c51ae21a385dd0a0464388ef1c2
kret <andreskee@gmail.com>
2011-05-12 18:47:52 +0200

    Modified BooksHelper#link_to_publication method to also work for Edition.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,7 @@
 module BooksHelper
   def link_to_publication(publication, options={})
+    publication = publication.publication if publication.is_a? Edition
     title = publication.title || publication.original_title || t('publication.message.title_unknown')
-    link_to "#{title}", publication, options
+    link_to title, publication, options
   end
 end

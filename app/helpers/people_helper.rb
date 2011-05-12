66e66b4d8a7c16a3401f4e45961b264aa9deb3b7
kret <andreskee@gmail.com>
2011-05-12 17:46:05 +0200

    Added pretty_print People helper.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,5 +1,17 @@
 module PeopleHelper
   def link_to_person(person, options={})
-    link_to "#{person.first_name} #{person.last_name}", person, options
+    link_to(person.display_name, person, options).html_safe
+  end
+
+  def pretty_print(people, options={})
+    separator = options.delete(:separator) || ", "
+    with_links = options.delete(:with_links)
+    with_links = true if with_links.nil?
+
+    if people.is_a? Person
+      with_links ? link_to_person(people, options).html_safe : people.display_name
+    else
+      with_links ? people.collect { |p| link_to_person(p, options) }.join(separator).html_safe : people.collect(&:display_name).join(separator)
+    end
   end
 end

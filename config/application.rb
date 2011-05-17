f2e1bc479d14e379813451af9baad672cb131c1d
kret <andreskee@gmail.com>
2011-05-18 01:19:52 +0200

    Swapping <div> for <span> in field_error_proc

--- config/application.rb
+++ config/application.rb
@@ -41,3 +41,5 @@ module BookWeb
     config.filter_parameters += [:password]
   end
 end
+
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| %(<span class="field_with_errors">#{html_tag}</span>).html_safe }
